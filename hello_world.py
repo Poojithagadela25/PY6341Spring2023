6a84bf619c0236c5b2e2659ee7c42c2f3176de64
Poojithagadela25 <poojitharaogadela@gmail.com>
2025-07-06 16:05:24 -0500

    Initial commit: added hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+# Full Name: <Poojitha rao gadela>
+# Assignment Due Date: 7/7/2025
+# MSITM 6341:
+# Assignment Title: Hello World
+# Helloworld.py
+print()
+print("----------------------------------------")
+print("Hello world!")
+print("----------------------------------------")
